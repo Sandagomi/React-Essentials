867e4756f3ca11556d3ef7eb862fb7c64cd5313e
sandagomi <sandagomi.v.i@gmail.com>
2025-11-28 12:11:37 +0530

    commit changes on state

--- src/components/state.tsx
+++ src/components/state.tsx
@@ -3,7 +3,31 @@ import { useState } from "react";
 
 const learnState = () => {
 
-    const state = useState();
+    const [count, setCount] = useState<number>(0);
+    const [email, setEmail] = useState<string>("enter your email");
+    const [password, setPassword] = useState<string>("enter your password");
+
+    return (
+        <div>
+            <h1>Learn State in React</h1>
+            <button onClick={() => setCount(count + 1)}>increase</button>
+
+            <form>
+                <input 
+                    type="email" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} 
+                />
+                <br />
+                <input 
+                    type="password" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} 
+                />
+                <br />
+            </form>
+        </div>
+    );
 
 }
 
